2ec9365df4c5af2f1e714fcc85f2886149d0c38f
Byronodhiambo <byronjonnes25@gmail.com>
2022-05-05 16:25:20 +0300

    odd occurances

--- faceid/views.py
+++ faceid/views.py
@@ -40,38 +40,37 @@ class users(APIView):
         Image.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-def comparefaces(request):
-    images = Image.objects.all()
-    for image in images:
-        print(image.photo)
-        
-    known_image = face_recognition.load_image_file("biden.png")
-    unknown_image = face_recognition.load_image_file("unknown.png")
-
-    try:
-        biden_encoding = face_recognition.face_encodings(known_image)[0]
-        unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
-        results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
-        print(results)
-
-    except:
-        print("Face not detected try again")
-
-
-   
-
-# comparefaces(request="")
-
-def captureimage(request):
+def captureimage():
     camera = cv2.VideoCapture(0)
     result, image = camera.read()
     if result:
-        cv2.imshow("your image", image)
+        # cv2.imshow("your image", image)
         cv2.imwrite("unknown.png", image)
-        cv2.waitKey(0)
-        cv2.destroyWindow("your image")
+        # cv2.waitKey(0)
+        # cv2.destroyWindow("your image")
+        return 'unknown.png'
     else:
         print("No image detected. Please! try again")
+
+
+def comparefaces():
+    images = Image.objects.all()
+    for image in images:
+        known_image = face_recognition.load_image_file(image.photo)
+        unknown_image = face_recognition.load_image_file(captureimage())
+
+        try:
+            biden_encoding = face_recognition.face_encodings(known_image)[0]
+            unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+            results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+            print(results) 
+
+        except:
+            print("Face not detected try again")
+
+# comparefaces(request="")
+
+
 
 # captureimage(request="")    
 # 
