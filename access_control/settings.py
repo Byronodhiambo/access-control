402e4bf5be16325f89ca693087f01ab6ba45d6fc
Byronodhiambo <byronjonnes25@gmail.com>
2022-05-10 00:45:26 +0300

    enroll user

--- access_control/settings.py
+++ access_control/settings.py
@@ -49,7 +49,16 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
+
+REST_FRAMEWORK = {
+    
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ]
+}
+
 
 ROOT_URLCONF = 'access_control.urls'
 
